8436d777c323df3531396afad29c3fa456413c7f
Lucía Revuelta <90416100+luciarevuelta@users.noreply.github.com>
2022-10-11 18:03:19 +0200

    update coffe_machine.py

--- coffe_machine.py
+++ coffe_machine.py
@@ -1,73 +1,70 @@
--- #VARIABLES GLOBALES--------
-# COINS
-QUARTER = 0.25
-DIME = 0.1
-NICKLE = 0.05
-PENNIE = 0.01
-# PRICE COFFEES
-PRICE_ESPRESSO = 1.5
-PRICE_LATTE = 2.5
-PRICE_CAPPUCCINO = 3
-
-------------------------------
-
-def decision_coffe():
-"""Función que pregunta al usuario qué tipo de café desea.
-"""
-
+def decision_coffee():
     coffes = ["espresso", "latte", "cappuccino"]
     while True:
         decision_user = input(
             "What would you like,(espresso/latte/cappuccino)?: ")
         if any(coffe in decision_user for coffe in coffes):
-            print(f"You choose a {decision_user} coffe. Great!")
-            return decision_user
+            drink = MENU[decision_user]  #almaceno dentro de la variable llamada bebida el diccionario con todos los ingredientes.
+            if is_resource_sufficient(drink["ingredients"]): #si hay recursos suficientes
+                payment = insert_coins()
+                if is_transaction_successful(payment, drink["cost"]):
+                    make_coffee(decision_user, drink["ingredients"])
+        elif decision_user == "off":
             break
-        print("Choose a correct coffe.")
+        elif decision_user == "report":
+            print(f"Water:{resources['water']} ml")
+            print(f"Milk: {resources['milk']} ml")
+            print(f"Coffee:{resources['coffee']} g")
+            print(f"money: ${profit}")  
+decision_coffee()
 
-decision_coffe()
+
+def is_resource_sufficient(order_ingredients):
+    """Devuelve True cuando se puede realizar el café (orden) y False cuando no.
+    """
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+    return True
 
 
-
+QUARTER = 0.25
+DIME = 0.1
+NICKLE = 0.05
+PENNIE = 0.01
 def insert_coins():
-    decision_user = decision_coffe()
-    if decision_user != "off":
         print("Please insert coins")
         while True:
             try:
-                quarters = float(input("How many quartes?"))
-                dimes = float(input("How many dimes?"))
-                nickles = float(input("How many nickles?"))
-                pennies = float(input("How many pennies"))
+                quarters = int(input("How many quartes?"))
+                dimes = int(input("How many dimes?"))
+                nickles = int(input("How many nickles?"))
+                pennies = int(input("How many pennies"))
             except ValueError:
                     print("That's not a float value! Repeat again!")
             else:
-                monetary_value = float(round(QUARTER * quarters + DIME * dimes + NICKLE * nickles + PENNIE * pennies,2))
-                return monetary_value, decision_user
-    
+                monetary_value = int(round(QUARTER * quarters + DIME * dimes + NICKLE * nickles + PENNIE * pennies,2))
+                return monetary_value
             #if (quarters.isnumeric() and dimes.isnumeric() and nickles.isnumeric() and pennies.isnumeric()):
                 #monetary_value = round(QUARTER * quarters + DIME * dimes + NICKLE * nickles + PENNIE * pennies,2)
                 #return monetary_value
+insert_coins()
 
-
-def enough_money():
-    decision_user = decision_coffe()
-    monetary_value = insert_coins()
-    if decision_user == "espresso" and monetary_value > PRICE_ESPRESSO:
-        change_espresso = float(round(monetary_value - PRICE_ESPRESSO))
-        print(f"Here is ${change_espresso} in change.")
-        print(f"He is your {decision_user}. Enjoy!")
-        return change_espresso
-    if decision_user == "latte" and monetary_value > PRICE_LATTE:
-        change_latte = float(round(monetary_value - PRICE_LATTE,2))
-        print(f"Here is ${change_latte} in change.")
-        print(f"He is your {decision_user}. Enjoy!")
-        return change_latte
-    if decision_user == "cappuccino" and monetary_value > PRICE_CAPPUCCINO:
-        change_cappuccino = float(round(monetary_value - PRICE_CAPUCCINO))
-        print(f"Here is ${change_cappuccino} in change.")
-        print(f"He is your {decision_user}. Enjoy!")
-        return change_capuccino
+def is_transaction_successful(money_received, drink_cost):
+    """Devuelve True cuando el pago es aceptado oo False si el dinero es insuficiente"""
+    if  money_received > drink_cost:
+        change = round(money_received - drink_cost,2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
     else:
-        print("Sorry taht's not enough money.")
-enought_money()
+        print("Sorry that's not enough money.")
+        return False
+    
+   def make_coffee(drink_name,order_ingredients):
+    "Required ingredients"
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}")
