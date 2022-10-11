cd7613ff9d098ae7e75f74bfbe84e755445fec82
Lucía Revuelta <90416100+luciarevuelta@users.noreply.github.com>
2022-10-11 18:04:50 +0200

    update caffe_machine.py

--- coffe_machine.py
+++ coffe_machine.py
@@ -68,3 +68,33 @@ def is_transaction_successful(money_received, drink_cost):
     for item in order_ingredients:
         resources[item] -= order_ingredients[item]
     print(f"Here is your {drink_name}")
+
+    
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,    
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {"water":700, "milk":500, "coffee":300}
